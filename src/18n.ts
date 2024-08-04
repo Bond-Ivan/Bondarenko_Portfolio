e20a07a414dd9bcdce2e796ffcb4555e5ab3f75b
ruabaie <ivan.bondarenko@raiffeisen.ru>
2024-08-04 18:45:44 +0300

    commit 1.1.3

--- src/18n.ts
+++ src/18n.ts
@@ -1,11 +1,11 @@
 import i18n from "i18next";
 import HttpApi from "i18next-http-backend";
-// import LanguageDetector from "i18next-browser-languagedetector";
+import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 i18n
   .use(HttpApi)
-  // .use(LanguageDetector)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     supportedLngs: ["ru", "en"],
